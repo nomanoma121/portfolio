d00f32a8ed9f8bb50e88bc24c5e0e5a94d10e1f9
nomanoma121 <takeuchi180121@gmail.com>
2025-04-29 03:45:55 +0900

    refact: format

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,10 +1,10 @@
 import { css } from "../../../styled-system/css";
 
 export const card = css({
-  width: "350px",
-  height: "280px",
-  borderRadius: "8px",
-  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
-  backgroundColor: "background",
-  padding: "16px",
+	width: "350px",
+	height: "280px",
+	borderRadius: "8px",
+	boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+	backgroundColor: "background",
+	padding: "16px",
 });
