c750fb30a7575b7d523d74ab1da0d7836949dca9
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2026-01-17 00:06:31 +0900

    chore: fontの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import { css } from "styled-system/css";
 import type { Metadata } from "next";
 import { Footer } from "../components/footer";
 import "highlight.js/styles/github-dark-dimmed.css";
+import { M_PLUS_Rounded_1c } from "next/font/google";
+
+const mPlusRounded = M_PLUS_Rounded_1c({
+	weight: ["400", "500", "700"],
+	subsets: ["latin"],
+	variable: "--font-m-plus-rounded",
+});
 
 export const metadata: Metadata = {
 	title: "nomanoma121",
@@ -15,9 +22,9 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="ja" className={css({ height: "100%" })}>
+		<html lang="ja" className={`${mPlusRounded.variable} ${css({ height: "100%" })}`}>
 			<body
-				className={css({
+				className={`${mPlusRounded.className} ${css({
 					backgroundColor: "background",
 					minHeight: "100vh",
 					display: "flex",
@@ -25,7 +32,7 @@ export default function RootLayout({
 					margin: "0",
 					padding: "0",
 					height: "100%",
-				})}
+				})}`}
 			>
 				<div
 					className={css({
