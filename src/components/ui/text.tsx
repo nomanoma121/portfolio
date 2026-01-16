b449c4ded140a6b04d45411927ffab1d10599036
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2026-01-16 16:01:34 +0900

    chore: 使っていないファイル削除

--- src/components/ui/text.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentProps } from 'react'
-import { styled } from 'styled-system/jsx'
-import { type TextVariantProps, text } from 'styled-system/recipes'
-import type { StyledComponent } from 'styled-system/types'
-
-type Props = TextVariantProps & { as?: React.ElementType }
-
-export type TextProps = ComponentProps<typeof Text>
-export const Text = styled('p', text) as StyledComponent<'p', Props>
